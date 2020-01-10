058b4d6596b99c454d3737a77cb0e0f84a7b0966
Samuel Alves <samuel241296@hotmail.com>
2020-01-10 13:09:13 -0300

    useLocation hook e google geolocation adicionados ao projeto. Novo build e novo deploy ao firebase

--- src/container/LandingPage/LandingPage.js
+++ src/container/LandingPage/LandingPage.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import { usePosition } from 'use-position';
 
 import Paper from '@material-ui/core/Paper';
 
@@ -6,6 +8,19 @@ import SearchForm from '../../components/SearchForm/SearchForm';
 
 const LandingPage = props => {
   const [searchInput, setSearchInput] = useState('');
+  const { latitude, longitude } = usePosition();
+
+
+  useEffect(()=>{
+    if(latitude !== undefined){
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAoZ_LibsLiGtHAp7wIS-5O9faTMkuO2yI`)
+    .then( (response)=>{
+      setSearchInput(response.data.results[0].address_components[3].long_name)
+    })
+    .catch((error)=>{
+    })
+  }
+  },[latitude,longitude]);
 
   const searchHandler = (event) => {
     event.preventDefault();
